19d010a8c921bab60967190ae6e370f81df43809
Michael Chan <michael-za.chan@ubs.com>
2019-09-17 13:18:26 +0800

    Initial Commit

---
+++ QuoteFetcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuoteFetcher
+//
+//  Created by Michael on 17/9/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
